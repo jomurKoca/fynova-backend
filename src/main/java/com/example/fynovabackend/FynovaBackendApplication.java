3dd64813b0fac8aa8574b7778e49b3458b8e1c8b
Ömür Koca <93597251+jomurKoca@users.noreply.github.com>
2025-07-13 19:30:49 +0200

    first commit

---
+++ src/main/java/com/example/fynovabackend/FynovaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.fynovabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FynovaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FynovaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fynovabackend/FynovaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fynovabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FynovaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
